5034cf6b9608e653a63eb45e1f6bbdff7ef66570
Crusader-RG <justin.leeper92@gmail.com>
2024-06-01 18:52:41 -0600

    complete takes in string

--- python/credit_check.py
+++ python/credit_check.py
@@ -1,5 +1,29 @@
 def credit_check(str):
-    pass
+    #create int array from string
+    num_list = [int(num) for num in str]
+    sum = 0
+
+    #loop through and double every other
+    for i in range(0, len(num_list),2):
+        num_list[i] = num_list[i] * 2
+
+    #loop through and sum digits greater than 9
+    for i in range(0, len(num_list)):
+        if num_list[i] > 9:
+            remain = num_list[i] % 10
+            num_list[i] = ((num_list[i] - remain)/10) + remain
+    
+    #loop through and sum 
+    for i in range(0, len(num_list)):
+        sum += num_list[i]
+
+    #check valid
+    if (sum % 10 == 0):
+        return "The number is valid!"
+    else:
+        return "The number is invalid!"
+    
+
 
 # Your Luhn Algorithm Here
 # Expected Output:
